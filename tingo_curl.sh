61b24f977fdcf83448b6923893124eb778de284d
CrioUser <crio-user@criodo.com>
2023-04-22 22:46:09 +0530

    commit for resassesments

--- tingo_curl.sh
+++ tingo_curl.sh
@@ -1,2 +1,3 @@
 curl -v https://api.tiingo.com/tiingo/daily/AAPL/prices?token=8d1051d60d86c0c0346a54396a53d6801c27cc16
 curl -v https://api.tiingo.com/tiingo/daily/GOOGL/prices?token=8d1051d60d86c0c0346a54396a53d6801c27cc16
+curl -v https://api.tiingo.com/tiingo/daily/MSFT/prices?token=8d1051d60d86c0c0346a54396a53d6801c27cc16
