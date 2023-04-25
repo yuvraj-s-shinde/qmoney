7187c936b6b11a4a4f3d8368bfe60904588f4bbf
CrioUser <crio-user@criodo.com>
2023-04-26 00:10:43 +0530

    for reassessment

--- tingo_curl.sh
+++ tingo_curl.sh
@@ -1,3 +1,2 @@
 curl -v https://api.tiingo.com/tiingo/daily/AAPL/prices?token=8d1051d60d86c0c0346a54396a53d6801c27cc16
 curl -v https://api.tiingo.com/tiingo/daily/GOOGL/prices?token=8d1051d60d86c0c0346a54396a53d6801c27cc16
-curl -v https://api.tiingo.com/tiingo/daily/MSFT/prices?token=8d1051d60d86c0c0346a54396a53d6801c27cc16
